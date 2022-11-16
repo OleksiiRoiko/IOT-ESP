cae61415984c56fef095a4c3cfaeb5a975fe8ae8
OleksiiRoiko <114649848+OleksiiRoiko@users.noreply.github.com>
2022-11-16 12:14:29 +0100

    Update form.php

--- form.php
+++ form.php
@@ -1,7 +1,43 @@
+<?php
+ob_start();
+session_start();
+require_once 'dbconnect.php';
+
+// if session is set direct to index
+if (isset($_SESSION['user'])) {
+    header("Location: index.php");
+    exit;
+}
+
+if (isset($_POST['btn-login'])) {
+    $email = $_POST['email'];
+    $upass = $_POST['pass'];
+
+    $password = hash('sha256', $upass); // password hashing using SHA256
+    $stmt = $conn->prepare("SELECT id, username, password FROM users WHERE email= ?");
+    $stmt->bind_param("s", $email);
+    /* execute query */
+    $stmt->execute();
+    //get result
+    $res = $stmt->get_result();
+    $stmt->close();
+
+    $row = mysqli_fetch_array($res, MYSQLI_ASSOC);
+
+    $count = $res->num_rows;
+    if ($count == 1 && $row['password'] == $password) {
+        $_SESSION['user'] = $row['id'];
+        header("Location: index.php");
+    } elseif ($count == 1) {
+        $errMSG = "Bad password";
+    } else $errMSG = "User not found";
+}
+?>
+
 <!DOCTYPE html>
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-    <title>Registration</title>
+    <title>Login</title>
     <link rel="stylesheet" href="assets/css/bootstrap.min.css" type="text/css"/>
     <link rel="stylesheet" href="assets/css/style.css" type="text/css"/>
 </head>
@@ -15,7 +51,7 @@
             <div class="col-md-12">
 
                 <div class="form-group">
-                    <h2 class="">Register for our Website</h2>
+                    <h2 class="">Login:</h2>
                 </div>
 
                 <div class="form-group">
@@ -27,7 +63,7 @@
 
                     ?>
                     <div class="form-group">
-                        <div class="alert alert-<?php echo ($errTyp == "success") ? "success" : $errTyp; ?>">
+                        <div class="alert alert-danger">
                             <span class="glyphicon glyphicon-info-sign"></span> <?php echo $errMSG; ?>
                         </div>
                     </div>
@@ -37,33 +73,16 @@
 
                 <div class="form-group">
                     <div class="input-group">
-                        <span class="input-group-addon"><span class="glyphicon glyphicon-user"></span></span>
-                        <input type="text" name="uname" class="form-control" placeholder="Enter Username" required/>
-                    </div>
-                </div>
-
-                <div class="form-group">
-                    <div class="input-group">
                         <span class="input-group-addon"><span class="glyphicon glyphicon-envelope"></span></span>
-                        <input type="email" name="email" class="form-control" placeholder="Enter Email" required/>
+                        <input type="email" name="email" class="form-control" placeholder="Email" required/>
                     </div>
                 </div>
 
                 <div class="form-group">
                     <div class="input-group">
                         <span class="input-group-addon"><span class="glyphicon glyphicon-lock"></span></span>
-                        <input type="password" name="pass" class="form-control" placeholder="Enter Password"
-                               required/>
+                        <input type="password" name="pass" class="form-control" placeholder="Password" required/>
                     </div>
-                </div>
-
-                <div class="checkbox">
-                    <label><input type="checkbox" id="TOS" value="This"><a href="#">I agree with
-                            terms of service</a></label>
-                </div>
-
-                <div class="form-group">
-                    <button type="submit" class="btn    btn-block btn-primary" name="signup" id="reg">Register</button>
                 </div>
 
                 <div class="form-group">
@@ -71,7 +90,16 @@
                 </div>
 
                 <div class="form-group">
-                    <a href="login.php" type="button" class="btn btn-block btn-success" name="btn-login">Login</a>
+                    <button type="submit" class="btn btn-block btn-primary" name="btn-login">Login</button>
+                </div>
+
+                <div class="form-group">
+                    <hr/>
+                </div>
+
+                <div class="form-group">
+                    <a href="register.php" type="button" class="btn btn-block btn-danger"
+                       name="btn-login">Register</a>
                 </div>
 
             </div>
@@ -82,7 +110,5 @@
 </div>
 <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
 <script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
-<script type="text/javascript" src="assets/js/tos.js"></script>
-
 </body>
 </html>
