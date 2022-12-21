38707851afc6a26292626a3e98cc3f5978e99cc6
OleksiiRoiko <114649848+OleksiiRoiko@users.noreply.github.com>
2022-12-21 23:29:19 +0100

    Update get.php

--- esp_zadanie/get.php
+++ esp_zadanie/get.php
@@ -2,7 +2,6 @@
     extract($_REQUEST);
     $file=fopen("form-save.txt","w");
 
-    fwrite($file,"Uhol sa rovna: ");
     fwrite($file, $input1);
     fclose($file);
     header("location: index.php");
