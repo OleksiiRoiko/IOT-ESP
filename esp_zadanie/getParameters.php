a797e2ccdaf290f28ddd86472724dd8c02a3c520
OleksiiRoiko <114649848+OleksiiRoiko@users.noreply.github.com>
2022-12-22 00:51:18 +0100

    Update getParameters.php

--- esp_zadanie/getParameters.php
+++ esp_zadanie/getParameters.php
@@ -2,7 +2,7 @@
   $time = 0;
   $angle = 0;
   $file = fopen("Values.txt","w");
-  echo fwrite($file,$time . "\n");
-  echo fwrite($file,$angle . "\n");
+  fwrite($file,$time . "\n");
+  fwrite($file,$angle . "\n");
   fclose($file);
 ?>
