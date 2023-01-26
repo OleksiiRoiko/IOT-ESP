8165a9ab569b793e98025430dbc598f19b7a0687
OleksiiRoiko <114649848+OleksiiRoiko@users.noreply.github.com>
2023-01-26 08:54:51 +0100

    Update exam.php

--- exam.php
+++ exam.php
@@ -1,4 +1,4 @@
-<form action="exam.php" method="post">
+<form action="text.php" method="post">
   <input type="text" id="text" name="text" />
   <button type="submit" class="btn btn-outline-dark" id="submit-btn">Submit</button>
 </form>
